83addc067355e031dee5bb94aaddfbd44b6bd476
Bartlomiej Treska <bartlomiej.treska@gmail.com>
2024-02-02 10:11:12 +0100

    Add screenshoter

---
+++ src/screenshoter.py
@@ -0,0 +1,9 @@
+import pyscreenshot as ImageGrab
+
+class Screenshooter:
+
+    @staticmethod
+    def take_screenshot(x_start, y_start, x_end, y_end):
+        x_start, x_end = min(x_start, x_end), max(x_start, x_end)
+        y_start, y_end = min(y_start, y_end), max(y_start, y_end)
+        return ImageGrab.grab(bbox=(x_start, y_start, x_end, y_end))
