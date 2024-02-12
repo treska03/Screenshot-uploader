332fcd7b403d524be60094a8102bf31ac2fd7202
Bartlomiej Treska <bartlomiej.treska@gmail.com>
2024-02-12 20:37:28 +0100

    Restructured project furthermore

--- src/tests/listeners/shortcut_listener_test.py
+++ src/tests/listeners/shortcut_listener_test.py
@@ -1,12 +1,12 @@
 import unittest
-from src.listeners.shortcut_listener import Keyboard_listener
+from src.main.listeners.shortcut_listener import Keyboard_listener
 
 listener = Keyboard_listener()
 
 class TestListener(unittest.TestCase):
 
     def test_unpacking(self):
-        self.assertEqual(set(["shift", "alt_l", "cmd"]), listener.shortcuts)
+        self.assertEqual(set(["Key.alt", "Key.shift", "Key.cmd"]), listener._shortcuts)
 
 
 
